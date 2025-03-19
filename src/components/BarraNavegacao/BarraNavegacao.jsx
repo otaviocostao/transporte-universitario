174a45bdff2f2e52737c6c5fb2fdcdac2a7bc0d0
Otavio <dev.otaviocostao@gmail.com>
2025-03-19 14:59:47 -0300

    Criação da barra lateral e ajustes na barra de navegação

--- src/components/BarraNavegacao/BarraNavegacao.jsx
+++ src/components/BarraNavegacao/BarraNavegacao.jsx
@@ -1,17 +1,29 @@
+import { useState } from 'react';
 import './BarraNavegacao.css'
+import { BsList } from "react-icons/bs";
+import MenuLateral from '../MenuLateral/MenuLateral';
 
 const BarraNavegacao = () => {
+  
+  const [showMenu, setShowMenu] = useState(false);
+  
+  const handleMenuClick = () =>{
+    setShowMenu(true);
+  };
+  
+  const handleCloseMenu = () => {
+    setShowMenu(false);
+  };
   return (
     <div className="nav-bar">
         <h2>Transporte universitário</h2>
-        <div className='navbar-buttons'>
-            <ul className='navbar-list-buttons'>
-              <li><a href="" className='navbar-button'>Início</a></li>
-              <li><a href="" className='navbar-button'>Histórico</a></li>
-              <li><a href="" className='navbar-button'>Passagens</a></li>
-              <li><a href="" className='navbar-button'>Pagamentos</a></li>
-            </ul>
-        </div>
+        <button className='menu-button' onClick={() => handleMenuClick()}><BsList /></button>
+
+        {showMenu && (
+          <MenuLateral 
+          isOpen={showMenu}
+          onClose={handleCloseMenu}/>
+        )}
     </div>
   )
 }
